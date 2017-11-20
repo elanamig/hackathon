a4b16bbd5e6d1c375f0853e743fcf4694ba6be38
raytorres280 <raytorres280@gmail.com>
2017-11-20 14:48:28 -0500

    html

--- sorter.js
+++ sorter.js
@@ -4,38 +4,34 @@ function bubbleSort(array, callback) {
       swapped = false;
       for(var i = 0; i < array.length; i++) {
         if(array[i] && array[i + 1] && array[i] > array[i + 1]) {
-          swap(array, i, i + 1);
-          swapped = true;
-          callback(array);
+          setTimeout(function() {
+            swap(array, i, i + 1);
+            swapped = true;
+            callback(array);
+          }, 2000)
         }
       }
     } while(swapped);
     return array;
   }
-  
-function swap (array, i1, i2) {
-    let temp = array[i1];
-    array[i1]= array[i2];
-    array[i2] = temp;
-}
 
 
 function mergeSort(arr)
 {
     if (arr.length < 2)
         return arr;
- 
+
     var middle = parseInt(arr.length / 2);
     var left   = arr.slice(0, middle);
     var right  = arr.slice(middle, arr.length);
- 
+
     return merge(mergeSort(left), mergeSort(right));
 }
- 
+
 function merge(left, right)
 {
     var result = [];
- 
+
     while (left.length && right.length) {
         if (left[0] <= right[0]) {
             result.push(left.shift());
@@ -43,12 +39,12 @@ function merge(left, right)
             result.push(right.shift());
         }
     }
- 
+
     while (left.length)
         result.push(left.shift());
- 
+
     while (right.length)
         result.push(right.shift());
- 
+
     return result;
 }
